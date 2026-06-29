ffd6de9b20f666f4e8f6e51f05bb1d7ed9d20a7a
mateolavarezaymar <mateojosuealvarezaymar@gmail.com>
2026-06-28 20:44:48 -0500

    IA

--- Solucion_Codigo/Proyecto_Taller07/src/Problema_2_EjecutorVideoClub.java
+++ Solucion_Codigo/Proyecto_Taller07/src/Problema_2_EjecutorVideoClub.java
@@ -13,84 +13,124 @@ Un videoclub dispone de una serie de películas que pueden estar en DVD
  * @author Mateo Alvarez
  * version 1.0
  */
-
-class Pelicula{
-    public String titulo, autor;
-    public int anio;
+class Pelicula {
+    private String titulo;
+    private String autor;
+    private int anio;
+ 
     public Pelicula(String titulo, String autor, int anio) {
         this.titulo = titulo;
         this.autor = autor;
         this.anio = anio;
     }
+ 
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+    public int getAnio() { return anio; }
+    public void setAnio(int anio) { this.anio = anio; }
+ 
     @Override
     public String toString() {
-        return "Pelicula{" + "titulo=" + titulo + ", autor=" + autor + ", anio=" + anio + '}';
+        return "Pelicula{titulo=" + titulo + ", autor=" + autor + ", anio=" + anio + "}";
     }
 }
-class Soporte{
-    public Pelicula pelicula;
-    public int cantidad;
-    public double precio, costoAlquiler;
+ 
+class Soporte {
+    private Pelicula pelicula;
+    private int cantidad;
+    private double precio;
+    protected double costoAlquiler;
+ 
     public Soporte(Pelicula pelicula, int cantidad, double precio) {
         this.pelicula = pelicula;
         this.cantidad = cantidad;
         this.precio = precio;
     }
+ 
+    public Pelicula getPelicula() { return pelicula; }
+    public void setPelicula(Pelicula pelicula) { this.pelicula = pelicula; }
+    public int getCantidad() { return cantidad; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+    public double getCostoAlquiler() { return costoAlquiler; }
+ 
     public double calcularCostoAlquiler() {
-        this.costoAlquiler = (this.cantidad * this.precio);
+        this.costoAlquiler = this.cantidad * this.precio;
         return this.costoAlquiler;
     }
+ 
     @Override
     public String toString() {
-        return "Soporte{" + "pelicula=" + pelicula + ", cantidad=" + cantidad + ", precio=" + precio + ", costoAlquiler=" + costoAlquiler + '}';
+        return "Soporte{pelicula=" + pelicula + ", cantidad=" + cantidad +
+               ", precio=" + precio + ", costoAlquiler=" + costoAlquiler + "}";
     }
 }
-class Dvd extends Soporte{
-    public double porcentajeRecargo;
-    public String Idiomas[];
-    public Dvd(double porcentajeRecargo, String[] Idiomas, Pelicula pelicula, int cantidad, double precio) {
+ 
+class Dvd extends Soporte {
+    private double porcentajeRecargo;
+    private String[] idiomas;
+ 
+    public Dvd(double porcentajeRecargo, String[] idiomas, Pelicula pelicula, int cantidad, double precio) {
         super(pelicula, cantidad, precio);
         this.porcentajeRecargo = porcentajeRecargo;
-        this.Idiomas = Idiomas;
+        this.idiomas = idiomas;
     }
-    public double calcularCostoAlquiler(){
-        this.costoAlquiler = super.calcularCostoAlquiler() + (this.costoAlquiler * (this.porcentajeRecargo / 100)) ;
+ 
+    public double getPorcentajeRecargo() { return porcentajeRecargo; }
+    public void setPorcentajeRecargo(double porcentajeRecargo) { this.porcentajeRecargo = porcentajeRecargo; }
+    public String[] getIdiomas() { return idiomas; }
+    public void setIdiomas(String[] idiomas) { this.idiomas = idiomas; }
+ 
+    @Override
+    public double calcularCostoAlquiler() {
+        this.costoAlquiler = super.calcularCostoAlquiler() + (this.costoAlquiler * (this.porcentajeRecargo / 100));
         return this.costoAlquiler;
     }
-
+ 
     @Override
     public String toString() {
-        return "Dvd{" + "porcentajeRecargo=" + porcentajeRecargo + ", Idiomas=" + Arrays.toString(Idiomas) + '}' + super.toString();
+        return "Dvd{porcentajeRecargo=" + porcentajeRecargo + ", Idiomas=" + Arrays.toString(idiomas) + "}" + super.toString();
     }
-    
 }
-class Vhs extends Soporte{
-    public String Idioma;
-    public Vhs(String Idioma, Pelicula pelicula, int cantidad, double precio) {
+ 
+class Vhs extends Soporte {
+    private String idioma;
+ 
+    public Vhs(String idioma, Pelicula pelicula, int cantidad, double precio) {
         super(pelicula, cantidad, precio);
-        this.Idioma = Idioma;
+        this.idioma = idioma;
     }
+ 
+    public String getIdioma() { return idioma; }
+    public void setIdioma(String idioma) { this.idioma = idioma; }
+ 
     @Override
     public String toString() {
-        return "Vhs{" + "Idioma=" + Idioma + '}' + super.toString();
+        return "Vhs{Idioma=" + idioma + "}" + super.toString();
     }
-    
 }
+ 
 public class Problema_2_EjecutorVideoClub {
     public static void main(String[] args) {
-        String Idiomas[] = {"ESP" ,"ENG"};
+        String[] idiomas = {"ESP", "ENG"};
         Pelicula peli = new Pelicula("El mundial", "Mateo", 2026);
-        Dvd dvd1 = new Dvd(10, Idiomas, peli, 2, 10);
+ 
+        Dvd dvd1 = new Dvd(10, idiomas, peli, 2, 10);
         dvd1.calcularCostoAlquiler();
         System.out.println(dvd1);
-        Vhs vhs1 = new Vhs(Idiomas[0], peli, 2, 10);
+ 
+        Vhs vhs1 = new Vhs(idiomas[0], peli, 2, 10);
         vhs1.calcularCostoAlquiler();
         System.out.println(vhs1);
     }
 }
 /**
  * run:
-   Dvd{porcentajeRecargo=10.0, Idiomas=[ESP, ENG]}Soporte{pelicula=Pelicula{titulo=El mundial, autor=Mateo, anio=2026}, cantidad=2, precio=10.0, costoAlquiler=22.0}
-   Vhs{Idioma=ESP}Soporte{pelicula=Pelicula{titulo=El mundial, autor=Mateo, anio=2026}, cantidad=2, precio=10.0, costoAlquiler=20.0}
-   BUILD SUCCESSFUL (total time: 0 seconds)
+Dvd{porcentajeRecargo=10.0, Idiomas=[ESP, ENG]}Soporte{pelicula=Pelicula{titulo=El mundial, autor=Mateo, anio=2026}, cantidad=2, precio=10.0, costoAlquiler=22.0}
+Vhs{Idioma=ESP}Soporte{pelicula=Pelicula{titulo=El mundial, autor=Mateo, anio=2026}, cantidad=2, precio=10.0, costoAlquiler=20.0}
+BUILD SUCCESSFUL (total time: 0 seconds)
+
  */
